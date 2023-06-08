3f458f2400b2c4c096bfcbf5a0aa5e740f2495cb
guya <m15715893725@163.com>
2023-06-08 17:51:11 +0800

    封装spinner组件

---
+++ guya-blog/src/components/common/Spinner/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="props" class="loading">this is loading animation</div>
+</template>
+
+<script setup lang="ts" props="loading">
+const props = withDefaults(defineProps<{ loading?: boolean }>(), {
+  loading: false,
+});
+</script>
+
+<style lang="scss" scoped>
+.loading {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 999;
+}
+</style>
